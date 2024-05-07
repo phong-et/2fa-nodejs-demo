81218f5d992b4248afc6a6678baa10b5b9046938
phillip <phillip7.et@gmail.com>
2024-05-07 15:16:27 +0700

    2nd commit

---
+++ 2fa.js
@@ -0,0 +1,9 @@
+﻿const speakeasy = require('speakeasy');
+const QRCode = require('qrcode')
+var secret = speakeasy.generateSecret({
+    name : 'phillip'
+});
+console.log(secret)
+QRCode.toDataURL(secret.otpauth_url, function (err, data_url) {
+    console.log(data_url);
+});
---
+++ verify.js
@@ -0,0 +1,8 @@
+﻿const speakeasy = require('speakeasy')
+
+var verify = speakeasy.totp.verify({
+    secret: '[kQ3AorO>$$Za3A!3BMeLCWAVd7>KrvX',
+    encoding: 'ascii',
+    token:'344217'
+})
+console.log(verify);
